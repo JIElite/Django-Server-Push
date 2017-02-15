059b3572f93e3013d5164c302939914757589cdb
JIElite <ita3051@gmail.com>
2017-02-16 03:04:28 +0800

    add topic cloud

--- project/word_cloud/views.py
+++ project/word_cloud/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def word_cloud(request):
     return render(request, 'index.html')
+
+def topic_cloud(request):
+    return render(request, 'topic_cloud.html')
